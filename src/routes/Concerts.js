3e89680c9ce199dc8f848cd8c3d46769cc94c694
Ruby Edge <valeoks21@gmail.com>
2020-06-11 15:57:34 +0300

    emulate fake data loading

--- src/routes/Concerts.js
+++ src/routes/Concerts.js
@@ -1,91 +1,81 @@
 import React, { Component } from "react";
 import Carousel from "nuka-carousel";
 
-const CAROUSEL_IMAGES = [ "conc1.png", "tour1.jpg", "tour2.jpg" ]
+const FAKE_DATA = [
+  {
+    img: "conc1.png",
+    title: "БИ 2",
+    body: 'Стадион "Динамо"',
+  },
+  {
+    img: "tour1.jpg",
+    title: "RAMMSTEIN",
+    body: 'EUROPE STADIUM TOUR',
+  },
+  {
+    img: "tour2.jpg",
+    title: "IRONMAIDEN",
+    body: '',
+  },
+]
 
 class Concerts extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      concerts: [],
+    }
+  }
+
   componentDidMount() {
+    const loadFakeData = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(FAKE_DATA);
+      }, 1000);
+    });
+
+    loadFakeData.then(data => this.setState({ concerts: data }))
+
     fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then(response => response.json())
-    .then(json => console.log(json))
+    .then(json => {
+      console.log(json)
+      // const data = JSON.parse(json);
+      //this.setState({ concerts: data });
+    })
   }
 
   render() {
+    const concerts = this.state.concerts;
+    if (!concerts.length) {
+      return <div>Loading...</div>
+    }
+
     return (
       <main>
         <h5> Some Concerts</h5>
 
         <Carousel>
-          { CAROUSEL_IMAGES.map((image, index) =>
-            <img key={index} className='mx-auto carousel_image' src={image} alt=''/>
+          { concerts.map((concert, index) =>
+            <img key={index} className='mx-auto carousel_image' src={concert.img} alt=''/>
           )}
         </Carousel>
 
-        <div className="card" style={{ width: "18rem" }}>
-          <img className="concerts w-100" src="./1.jpg" alt="concert1" />
-          <div className="card-body">
-            <h5 className="card-title">Concert</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="/" className="btn btn-success moren">
-              Concert
-            </a>
+        { concerts.map((concert, index) => (
+          <div key={index} className="card" style={{ width: "18rem" }}>
+            <img className="concerts w-100" src={concert.img} alt="concert1" />
+            <div className="card-body">
+              <h5 className="card-title">{concert.title}</h5>
+              <p className="card-text">
+                {concert.body}
+              </p>
+              <a href="/" className="btn btn-success moren">
+                {concert.title}
+              </a>
+            </div>
           </div>
-        </div>
-
-        <div className="card" style={{ width: "18rem" }}>
-          <img className="concerts w-100" src="./tour1.jpg" alt="tour1" />
-          <div className="card-body">
-            <h5 className="card-title">RAMMSTEIN EUROPE STADIUM TOUR</h5>
-            <p className="card-text">
-              17.07.2020 20:00 18+
-            </p>
-            <a href="/" className="btn btn-success moren">
-              Tours
-            </a>
-          </div>
-        </div>
-
-        <div className="card" style={{ width: "18rem" }}>
-          <img className="concerts w-100" src="./tour1.jpg" alt="tour1" />
-          <div className="card-body">
-            <h5 className="card-title">RAMMSTEIN EUROPE STADIUM TOUR</h5>
-            <p className="card-text">
-              17.07.2020 20:00 18+
-            </p>
-            <a href="/" className="btn btn-success moren">
-              Tours
-            </a>
-          </div>
-        </div>
-
-        <div className="card" style={{ width: "18rem" }}>
-          <img className="concerts w-100" src="./tour1.jpg" alt="tour1" />
-          <div className="card-body">
-            <h5 className="card-title">RAMMSTEIN EUROPE STADIUM TOUR</h5>
-            <p className="card-text">
-              17.07.2020 20:00 18+
-            </p>
-            <a href="/" className="btn btn-success moren">
-              Tours
-            </a>
-          </div>
-        </div>
-
-        <div className="card" style={{ width: "18rem" }}>
-          <img className="concerts w-100" src="./tour1.jpg" alt="tour1" />
-          <div className="card-body">
-            <h5 className="card-title">RAMMSTEIN EUROPE STADIUM TOUR</h5>
-            <p className="card-text">
-              17.07.2020 20:00 18+
-            </p>
-            <a href="/" className="btn btn-success moren">
-              Tours
-            </a>
-          </div>
-        </div>
+        )) }
 
       </main>
     );
